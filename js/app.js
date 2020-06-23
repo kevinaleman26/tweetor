6680c9707f43132fd6ecbb4fa3594028eefe217d
Kevin Ernesto Alemán Martínez <kevin.aleman@hotmail.com>
2020-06-22 22:55:29 -0500

    se coloca scope

--- js/app.js
+++ js/app.js
@@ -137,16 +137,29 @@ postBtn.on('click', function() {
 
 });
 
-window.addEventListener('beforeinstallprompt', event => {
-    event.preventDefault();
-    console.log(event.platforms);
-    event.userChoice.then(function(choiceResult) { 
-        console.log(choiceResult.outcome); // either "accepted" or "dismissed"
-      }, handleError); 
-    var button = document.querySelector('button');
-    button.removeAttribute('hidden');
-    button.addEventListener('click', () => {
-      event.prompt();
-      button.setAttribute('disabled', true);
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', function(event) {
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+});
+
+// Installation must be done by a user gesture! Here, the button click
+btnAdd.addEventListener('click', (e) => {
+    // hide our user interface that shows our A2HS button
+    btnAdd.style.display = 'none';
+    // Show the prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice
+    .then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+        } else {
+        console.log('User dismissed the A2HS prompt');
+        }
+        deferredPrompt = null;
     });
-  });
+});
